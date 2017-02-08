c4d54826174243d9e71b607ccc9a6ac2b84c99d3
Nicolas L <legat.nicolas@gmail.com>
2017-02-08 01:22:34 +0100

    Initial commit

---
+++ Contextualizer/AddNew_UC.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Contextualizer
+{
+    /// <summary>
+    /// Logique d'interaction pour AddNew_UC.xaml
+    /// </summary>
+    public partial class AddNew_UC : UserControl
+    {
+        public AddNew_UC()
+        {
+            InitializeComponent();
+        }
+
+        private void ExecPath_Click_1(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            Nullable<bool> res = dlg.ShowDialog();
+            if (res == true)
+            {
+                this.ExecPathTextBox.Text = dlg.FileName;
+            }
+        }
+
+        private void ValideAdd_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string rep = (string)Registry.GetValue(@"HKEY_CLASSES_ROOT\Directory\Background\shell\powershella", "", null);
+                if (rep != null)
+                {
+                    this.NameAdd.Text = rep;
+                }
+                else
+                {
+                    throw new Exception("La clé de registre n'existe pas");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur : " + ex.Message, "Erreur");
+            }
+        }
+    }
+}
---
+++ Contextualizer/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+
+
+namespace Contextualizer
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.TransitionBox.Content = new ShowAll_UC();
+        }
+
+        private void Minimize_MouseLeftButtonUp_1(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Exit_MouseLeftButtonUp_1(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WindowName_MouseMove_1(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed )
+            {
+                this.DragMove();
+            }
+        }
+
+        private void ShowAll_MouseLeftButtonUp_1(object sender, MouseButtonEventArgs e)
+        {
+            this.TransitionBox.Content = new ShowAll_UC();
+        }
+
+        private void AddNew_MouseLeftButtonUp_1(object sender, MouseButtonEventArgs e)
+        {
+            this.TransitionBox.Content = new AddNew_UC();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Closing -= Window_Closing;
+            e.Cancel = true;
+            var anim = new System.Windows.Media.Animation.DoubleAnimation(0, (Duration)TimeSpan.FromMilliseconds(300));
+            anim.Completed += (s, _) => this.Close();
+            this.BeginAnimation(UIElement.OpacityProperty, anim);
+        }
+
+        private void AddTerminal_MouseLeftButtonUp_1(object sender, MouseButtonEventArgs e)
+        {
+            this.TransitionBox.Content = new AddTerminal();
+        }
+    }
+}
